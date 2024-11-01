b8365928781442f99c119c2644137df44242b5b3
Caio Carvalho <21188280+ccrvlh@users.noreply.github.com>
2024-11-01 02:44:21 -0300

    feat: wrap AppContent in config context provider

---
+++ webview/src/AppContent.tsx
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useState } from "react"
+import { useEvent } from "react-use"
+import { ExtensionMessage } from "../../src/shared/ExtensionMessage"
+import ChatView from "./components/chat/ChatView"
+import HistoryView from "./components/history/HistoryView"
+import SettingsView from "./components/settings/SettingsView"
+import WelcomeView from "./components/welcome/WelcomeView"
+import { useExtensionState } from "./context/ExtensionStateContext"
+import { vscode } from "./utils/vscode"
+
+export function AppContent() {
+  const { didHydrateState, showWelcome, shouldShowAnnouncement } = useExtensionState()
+  const [showSettings, setShowSettings] = useState(false)
+  const [showHistory, setShowHistory] = useState(false)
+  const [showAnnouncement, setShowAnnouncement] = useState(false)
+
+  const handleMessage = useCallback((e: MessageEvent) => {
+    const message: ExtensionMessage = e.data
+    switch (message.type) {
+      case "action":
+        switch (message.action!) {
+          case "settingsButtonClicked":
+            setShowSettings(true)
+            setShowHistory(false)
+            break
+          case "historyButtonClicked":
+            setShowSettings(false)
+            setShowHistory(true)
+            break
+          case "chatButtonClicked":
+            setShowSettings(false)
+            setShowHistory(false)
+            break
+        }
+        break
+    }
+  }, [])
+
+  useEvent("message", handleMessage)
+
+  useEffect(() => {
+    if (shouldShowAnnouncement) {
+      setShowAnnouncement(true)
+      vscode.postMessage({ type: "didShowAnnouncement" })
+    }
+  }, [shouldShowAnnouncement])
+
+  if (!didHydrateState) {
+    return null
+  }
+
+  if (showWelcome) {
+    return <WelcomeView />
+  }
+
+  return (
+    <>
+      {showSettings && <SettingsView onDone={() => setShowSettings(false)} />}
+      {showHistory && <HistoryView onDone={() => setShowHistory(false)} />}
+      {/* Do not conditionally load ChatView, it's expensive and there's state we don't want to lose (user input, disableInput, askResponse promise, etc.) */}
+      <ChatView
+        showHistoryView={() => {
+          setShowSettings(false)
+          setShowHistory(true)
+        }}
+        isHidden={showSettings || showHistory}
+        showAnnouncement={showAnnouncement}
+        hideAnnouncement={() => {
+          setShowAnnouncement(false)
+        }}
+      />
+    </>
+  )
+}
