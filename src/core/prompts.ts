280120904bf5487db09cbf8427b2d8ddde2a7448
Caio Carvalho <21188280+ccrvlh@users.noreply.github.com>
2024-11-01 01:03:36 -0300

    feat: improve write to file prompt

--- src/core/prompts.ts
+++ src/core/prompts.ts
@@ -206,8 +206,8 @@ const READ_FILE_TOOL = (cwd: string) => dedent`
 
 const WRITE_TO_FILE_TOOL = (cwd: string) => dedent`
   ## write_to_file
-  Description: Request to write content to a file at the specified path. If the file exists, it will be overwritten with the provided content. If the file doesn't exist, it will be created. This tool will automatically create any directories needed to write the file.
-  Only use this tool as a last resort. search_replace and insert_code_block tools are preferred.
+  Description: Request to rewrite the full file content to a file at the specified path. If the file exists, it will be overwritten with the provided content. If the file doesn't exist, it will be created. This tool will automatically create any directories needed to write the file.
+  Only use this tool as a last resort. search_replace and insert_code_block tools are preferred for being more efficient ways to edit files.
   Parameters:
   - path: (required) The path of the file to write to (relative to the current working directory ${cwd.toPosix()})
   - content: (required) The content to write to the file. ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified.
